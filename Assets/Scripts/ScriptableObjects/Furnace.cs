a068f5bc0885ff86fdeb745273a01dd5717bddd4
Oscar Villarreal <30502136+TheScarred@users.noreply.github.com>
2020-02-01 12:35:07 -0600

    Scriptable Object de Furnace
    a

---
+++ Assets/Scripts/ScriptableObjects/Furnace.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Furnace 1", menuName = "Furnace")]
+public class Furnace : ScriptableObject
+{
+    public Transform spawnPoint;
+    public Item materialNeeded, result;
+}
